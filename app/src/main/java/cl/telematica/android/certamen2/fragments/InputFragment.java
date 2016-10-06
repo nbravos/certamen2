fc697335b421307c8b5d000ae0df1d280c2995b2
nbravos <natalia.bravo.suazo@gmail.com>
2016-10-06 17:15:13 -0300

    1er commit por si acaso

--- app/src/main/java/cl/telematica/android/certamen2/fragments/InputFragment.java
+++ app/src/main/java/cl/telematica/android/certamen2/fragments/InputFragment.java
@@ -6,10 +6,23 @@ import android.support.v4.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
+import android.widget.EditText;
+import cl.telematica.android.certamen2.HttpServerConnection;
+import android.os.AsyncTask;
+import android.widget.TextView;
+
+import org.json.JSONException;
+import org.json.JSONObject;
 
 import cl.telematica.android.certamen2.R;
 
 public class InputFragment extends Fragment {
+
+    EditText eText1;
+    EditText eText2;
+    Button button_1;
+    TextView tText;
 
     /**
      * New instance of InputFragment
@@ -31,10 +44,63 @@ public class InputFragment extends Fragment {
         super.onCreateView(inflater, container, savedInstanceState);
         View mainView = inflater.inflate(R.layout.fragment_input, null);
 
+
         /*
          * Aquí es donde deben hacer el link a los elementos del layout fragment_input,
          * y donde prácticamente se debe hacer el desarrollo
         */
+
+        button_1 = (Button) mainView.findViewById(R.id.button1);
+
+        eText1 = (EditText) mainView.findViewById(R.id.name_input);
+        eText2 = (EditText) mainView.findViewById(R.id.lastname_input);
+
+        tText = (TextView)mainView.findViewById(R.id.Joke_text);
+
+        final String str1 = eText1.getText().toString();
+        final String str2 = eText2.getText().toString();
+
+
+        button_1.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                AsyncTask<Void, Void, String> task = new AsyncTask<Void, Void, String>() {
+
+                    @Override
+                    protected void onPreExecute(){
+
+                    }
+
+                    @Override
+                    protected String doInBackground(Void... params) {
+                        String resultado = new HttpServerConnection().connectToServer("http://api.icndb.com/jokes/random?firstName="+str1+"&lastName="+str2+"", 15000);
+                        return resultado;
+                    }
+
+                    @Override
+                    protected void onPostExecute(String result) {
+                        if(result != null){
+                            try {
+                                JSONObject objeto = new JSONObject(result);
+                                String joke = objeto.getString("joke");
+                                tText.setText(joke);
+
+
+                            } catch (JSONException e) {
+                                e.printStackTrace();
+                            }
+
+
+                        }
+                    }
+                };
+
+                task.execute();
+            }
+        });
+
+
+
 
         return mainView;
     }
